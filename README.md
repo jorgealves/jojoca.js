dedb4b7a9d4d3e8bcfa6bd7ec0b568c8245141c5
Jorge André Alves <jorgeandrealves@gmail.com>
2017-10-19 16:34:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jojoca.js
+A JS Library that simply doesn't fail
---
+++ jojoca.js
@@ -0,0 +1,6 @@
+var Jojoca = function() {};
+
+Jojoca.prototype.execute = function(){
+	console.log("See? I don't fail!");
+	return arguments;
+}

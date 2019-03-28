0fa63eae694c950ae3ba10e0357751528ca74484
Carlos Batman <Painatalman@users.noreply.github.com>
2019-03-28 15:10:40 +0000

    add new method to get a non-erroring function (#2)

--- jojoca.js
+++ jojoca.js
@@ -4,3 +4,37 @@ Jojoca.prototype.execute = function(){
 	console.log("See? I don't fail!");
 	return arguments;
 }
+
+/* 
+ * Returns a modified version of the original function that, if it ends up throwing an error, returns a console.log instead! 
+ * 
+ * @param {function} originalFunction the original function to be wrapped
+ * @returns function a modified version of the function provided in the parameters
+ * 
+ * @example
+ * function runAndSum(func, a, b) {
+ *  console.log(a+b);
+    func()
+ * }
+ * 
+ * let aJojoca = new Jojoca();
+ * let jw = aJojoca.getWrappedFunction(runAndSum);
+ * 
+ * // works
+ * runAndSum(function(){console.log('yeah')}, 1, 2);
+ * // works all the same
+ * jw(function(){console.log('yeah')}, 1, 2);
+ * // fails
+ * // runAndSum(1, 1, 2);
+ * // DOES NOT FAIL
+ * jw(1, 1, 2);
+ */
+Jojoca.prototype.getWrappedFunction = function(originalFunction) {
+    return function() {
+        try {
+            originalFunction.apply(this, arguments);
+        } catch(e) {
+            console.log(`aconteceu "${e}", mas Jojoca NÃO FALHA!`);
+        }
+    }
+}
